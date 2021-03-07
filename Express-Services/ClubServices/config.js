2c18fec6ea99f59485c2995284668a5abdd01fb7
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2021-03-07 15:35:17 +0530

    changed agora credentials, these credentials are for project name FLOCKTALE in agora console (with flocktale id)

--- Express-Services/ClubServices/config.js
+++ Express-Services/ClubServices/config.js
@@ -1,76 +1,76 @@
-const AWS = require('aws-sdk');
+    const AWS = require('aws-sdk');
 
-AWS.config.update({
-    region: "us-east-1",
-    // endpoint: "http://localhost:3000"        //this endpoint is used in case of local dynamodb on pc.
-    // endpoint: "http://dynamodb.us-east-1.amazonaws.com"  // by default it is set according to region
-});
+    AWS.config.update({
+        region: "us-east-1",
+        // endpoint: "http://localhost:3000"        //this endpoint is used in case of local dynamodb on pc.
+        // endpoint: "http://dynamodb.us-east-1.amazonaws.com"  // by default it is set according to region
+    });
 
-const dynamoClient = new AWS.DynamoDB.DocumentClient();
-const s3 = new AWS.S3();
-const sns = new AWS.SNS();
+    const dynamoClient = new AWS.DynamoDB.DocumentClient();
+    const s3 = new AWS.S3();
+    const sns = new AWS.SNS();
 
-const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: 'https://08oc4tw1hb.execute-api.us-east-1.amazonaws.com' + '/' + 'Dev'
-});
+    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: 'https://08oc4tw1hb.execute-api.us-east-1.amazonaws.com' + '/' + 'Dev'
+    });
 
-const imageUploadConstParams = {
-    ACL: 'public-read',
-    Bucket: 'mootclub-public',
-    // Body:            populate it 
-    // Key:             populate it
-};
-
-
-const agoraAppId = "7c3800483bbc473bbf341e1d68f04a40";
-const agoraPrimaryCertificate = "8b55b57e5db34a41bf974321c8671339";
+    const imageUploadConstParams = {
+        ACL: 'public-read',
+        Bucket: 'mootclub-public',
+        // Body:            populate it 
+        // Key:             populate it
+    };
 
 
-const myTable = "MyTable";
+    const agoraAppId = "f58d5e866a87498988cd3c138759bb2a";
+    const agoraPrimaryCertificate = "a420f033ec69472c885af6775c861701";
 
 
-const WsTable = 'WsTable';
-const wsInvertIndex = 'wsInvertIndex';
-const wsUserIdIndex = 'wsUserIdIndex';
+    const myTable = "MyTable";
 
 
-
-const clubCategoryIndex = "ClubCategoryIndex";
-
-const clubCreatorIdIndex = "ClubCreatorIdIndex";
-
-const sortKeyWithTimestampIndex = "SortKeyWithTimestampIndex";
-
-const usernameSortIndex = "UsernameSortIndex";
-const timestampSortIndex = "TimestampSortIndex";
-const audienceDynamicDataIndex = "AudienceDynamicDataIndex";
-const searchByUsernameIndex = "SearchByUsernameIndex";
+    const WsTable = 'WsTable';
+    const wsInvertIndex = 'wsInvertIndex';
+    const wsUserIdIndex = 'wsUserIdIndex';
 
 
 
-module.exports = {
-    dynamoClient,
-    s3,
-    sns,
-    apigwManagementApi,
+    const clubCategoryIndex = "ClubCategoryIndex";
 
-    imageUploadConstParams,
-    myTable,
+    const clubCreatorIdIndex = "ClubCreatorIdIndex";
 
-    WsTable,
-    wsInvertIndex,
-    wsUserIdIndex,
+    const sortKeyWithTimestampIndex = "SortKeyWithTimestampIndex";
 
-    clubCategoryIndex,
-    clubCreatorIdIndex,
-    sortKeyWithTimestampIndex,
+    const usernameSortIndex = "UsernameSortIndex";
+    const timestampSortIndex = "TimestampSortIndex";
+    const audienceDynamicDataIndex = "AudienceDynamicDataIndex";
+    const searchByUsernameIndex = "SearchByUsernameIndex";
 
-    usernameSortIndex,
-    timestampSortIndex,
-    audienceDynamicDataIndex,
-    searchByUsernameIndex,
 
-    agoraAppId,
-    agoraPrimaryCertificate,
-};
+
+    module.exports = {
+        dynamoClient,
+        s3,
+        sns,
+        apigwManagementApi,
+
+        imageUploadConstParams,
+        myTable,
+
+        WsTable,
+        wsInvertIndex,
+        wsUserIdIndex,
+
+        clubCategoryIndex,
+        clubCreatorIdIndex,
+        sortKeyWithTimestampIndex,
+
+        usernameSortIndex,
+        timestampSortIndex,
+        audienceDynamicDataIndex,
+        searchByUsernameIndex,
+
+        agoraAppId,
+        agoraPrimaryCertificate,
+    };
